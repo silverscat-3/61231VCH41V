b3b4d042cf52f98dfeec5dc69a77af18998359c0
silverscat-3 <silverscat_3@mail.sc3.fun>
2020-06-23 23:33:44 +0900

    コマンドライン引数で機能を分離

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -9,18 +10,35 @@ import (
 	"github.com/bluele/mecab-golang"
 	"github.com/silverscat-3/61231VCH41V/markov"
 	"github.com/silverscat-3/61231VCH41V/misskey"
-	flag "github.com/spf13/pflag"
 )
 
 func main() {
-	var (
-		misskeyToken = flag.String("misskey_token", "", "API token of Misskey")
-		misskeyHost  = flag.String("misskey_host", "", "URL of Misskey server")
-	)
+	if 2 > len(os.Args) {
+		printHelp()
+	}
 
-	flag.Parse()
+	switch os.Args[1] {
+	case "post":
+		if 4 > len(os.Args) {
+			log.Fatalln("Too few arguments.")
+		}
 
-	m, err := misskey.NewMisskey(*misskeyHost, *misskeyToken)
+		host := os.Args[2]
+		token := os.Args[3]
+
+		postNote(host, token)
+
+	case "help":
+		printHelp()
+
+	default:
+		fmt.Printf("%q is not valid command.\n", os.Args[1])
+		printHelp()
+	}
+}
+
+func postNote(host, token string) {
+	m, err := misskey.NewMisskey(host, token)
 	if nil != err {
 		log.Fatalln(err)
 	}
@@ -31,6 +49,20 @@ func main() {
 		log.Fatalln(err)
 	}
 	log.Printf("Posted!! %s\n", trimString(note))
+}
+
+func printHelp() {
+	fmt.Print(`
+61231VCH41V is shitpost maker.
+
+Usage:
+
+        61231VCH41V <command> [arguments]
+
+The commands are:
+post	post a shitpost
+`)
+	os.Exit(0)
 }
 
 func genString() string {
